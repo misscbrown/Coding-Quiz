221cdc08e7f1251c85b1b72d634243a1a7040023
cee breezy <djmisscbrown@hotmail.com>
2022-01-10 16:36:09 +0000

    Added divs to html buttons & functions in js

---
+++ Develop/script.js
@@ -0,0 +1,20 @@
+function startTimer() {
+
+}
+
+const startButton = document.getElementById("start-quiz")
+
+startButton.addEventListener('click', startQuiz)
+
+function startQuiz() {
+
+}
+
+function setNextQuestion() {
+
+}
+
+function selectAnswer() {
+
+}
+
