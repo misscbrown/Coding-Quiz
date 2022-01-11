3e88725968cd93a4bcaed6299a54e0b9d1b738f2
cee breezy <djmisscbrown@hotmail.com>
2022-01-11 14:21:07 +0000

    Added timer function to javascript

--- Develop/script.js
+++ Develop/script.js
@@ -1,20 +1,63 @@
-function startTimer() {
 
+//Timer elements from html, with message once timer complete
+var timerEl = document.getElementById('counting');
+var mainEl = document.getElementById('main');
+
+var message = 
+    "Time's up!";
+var words = message.split(' ');
+
+//Timer function
+function counting() {
+    var timeLeft = 90;
+
+    var timeInterval = setInterval(function () {
+        if(timeLeft > 1){
+            timerEl.textContent = timeLeft + ' seconds left 🕧'
+            timeLeft--
+        
+        }
+        else {
+            timerEl.textContent = ''
+            clearInterval(timeInterval)
+            displayMessage();
+        }
+    }, 1000);
 }
 
-const startButton = document.getElementById("start-quiz")
+function displayMessage() {
+    var wordCount = 0;
 
-startButton.addEventListener('click', startQuiz)
-
-function startQuiz() {
-
+    var msgInterval = setInterval(function () {
+        if (words[wordCount] === undefined) {
+            clearInterval(msgInterval);
+        } else {
+            mainEl.textContent = words[wordCount];
+            wordCount++;
+        }
+    }, 1000);
 }
 
-function setNextQuestion() {
+counting();
 
-}
 
-function selectAnswer() {
 
-}
 
+
+
+
+// function startTimer() {
+
+// }
+
+// const startButton = document.getElementById("start-quiz")
+
+// startButton.addEventListener('click', startQuiz)
+
+// function startQuiz() {
+
+// }
+
+// function setNextQuestion() {
+
+// }
