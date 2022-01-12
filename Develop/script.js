5b0a91660464330b1e7e8bb7a99ffa2afaa10d2c
cee breezy <djmisscbrown@hotmail.com>
2022-01-12 12:30:16 +0000

    Added question divs in html/added questions in js

--- Develop/script.js
+++ Develop/script.js
@@ -7,7 +7,7 @@ var message =
     "Time's up!";
 var words = message.split(' ');
 
-//Timer function
+//Timer function with display message
 function counting() {
     var timeLeft = 90;
 
@@ -40,7 +40,55 @@ function displayMessage() {
 
 counting();
 
+//Questions for quiz
 
+const questions = [
+    {
+        question: "What does HTML stand for?", 
+        answers: [
+            { text: 'Hyper Text Markup Language', correct: false },
+            { text: 'Hyped Type Marker Language', correct: false },
+            { text: 'Hover Text Markup Language', correct: false }
+        ]
+    },
+
+    {
+        question: "What does CSS stand for?",
+        answers: [
+            { text: 'Casanova Styling Shoes', correct: false },
+            { text: 'Cascading Style Sheets', correct: false },
+            { text: 'Cascading Sheet Sets', correct: false}
+        ]
+    },
+
+    {
+        question: "What is the meaning of const?",
+        answers: [
+            { text: 'Defines a variable as unchangeable', correct: false },
+            { text: 'Constantly typing code', correct: false },
+            { text: 'Constantly change a variable', correct: false}
+        ]
+    },
+
+    {
+        question: "Where do we link a javascript file?",
+        answers: [
+            { text: 'At the bottom of the CSS file', correct: false },
+            { text: 'At the top of the HTML file', correct: false },
+            { text: 'At the bottom of the HTML file', correct: false}
+        ]
+    },
+
+    {
+        question: "What is a Boolean?",
+        answers: [
+            { text: 'A variable that can have a true or false value', correct: false },
+            { text: 'A type of scary ghost', correct: false },
+            { text: 'A variable that is true, false or unsure', correct: false}
+        ]
+    },
+
+]
 
 
 
